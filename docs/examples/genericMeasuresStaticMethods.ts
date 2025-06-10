cbc9d005840741b99ca87532ca12af2ecb214fc5
Harper <git@harperandrews.org>
2025-06-10 14:43:57 -0500

    Make prettier happy

--- docs/examples/genericMeasuresStaticMethods.ts
+++ docs/examples/genericMeasuresStaticMethods.ts
@@ -1,4 +1,12 @@
-import { type BasisType, GenericMeasure, Mass, NumericOperations, Unit, createMeasureType, wrapUnaryFn } from "safe-units";
+import {
+    type BasisType,
+    GenericMeasure,
+    Mass,
+    NumericOperations,
+    Unit,
+    createMeasureType,
+    wrapUnaryFn,
+} from "safe-units";
 
 class WrappedNumber {}
 type WrappedMeasure<B extends BasisType, U extends Unit<B>> = GenericMeasure<WrappedNumber, B, U>;
